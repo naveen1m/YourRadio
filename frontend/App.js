c6ca084242e6a4439dd80cbfcb3b7e32f28727ce
naveen <stupiddint@gmail.com>
2023-10-19 09:25:57 +0530

    frontend initialised

---
+++ frontend/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Your Radio Project started!!</Text>
+      <Text>You can see live view on android emulator :) </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
