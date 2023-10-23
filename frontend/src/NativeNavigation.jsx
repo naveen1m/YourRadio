7f872d896b06ea05766e58742fe04e81e2e1d9b4
naveen <stupiddint@gmail.com>
2023-10-23 17:42:17 +0530

    app navigation

--- frontend/src/NativeNavigation.jsx
+++ frontend/src/NativeNavigation.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-
 import { NavigationContainer } from '@react-navigation/native';
+import { getHeaderTitle } from '@react-navigation/elements';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import { Home, SearchPage, Podcast, Notification, Profile } from './screens'
+import Header from './components/Header';
 
 
 // screen constants
@@ -13,45 +14,59 @@ const searchName = 'Search'
 const podcastName = 'Podcast'
 const notificationName = 'Notifications'
 
-const Tab = createBottomTabNavigator();
 
 function NativeNavigation() {
+    const Tab = createBottomTabNavigator();
+    // const Stack = createStackNavigator();
     return (
+        <>
+            <NavigationContainer>
+                <Tab.Navigator
+                    initialRouteName={homeName}
+                    screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
+                            let rn = route.name;
 
-        <NavigationContainer>
-            <Tab.Navigator
-                initialRouteName={homeName}
-                screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
-                        let rn = route.name;
+                            if (rn == homeName) {
+                                iconName = focused ? 'home' : 'home-outline'
+                            } else if (rn == searchName) {
+                                iconName = focused ? 'search' : 'search-outline'
+                            } else if (rn == podcastName) {
+                                iconName = focused ? 'radio' : 'radio-outline'
+                            } else if (rn == notificationName) {
+                                iconName = focused ? 'notifications' : 'notifications-outline'
+                            } else if (rn == profileName) {
+                                iconName = focused ? 'person' : 'person-outline'
+                            }
 
-                        if (rn == homeName) {
-                            iconName = focused ? 'home' : 'home-outline'
-                        } else if (rn == searchName) {
-                            iconName = focused ? 'search' : 'search-outline'
-                        } else if (rn == podcastName) {
-                            iconName = focused ? 'radio' : 'radio-outline'
-                        } else if (rn == notificationName) {
-                            iconName = focused ? 'notifications' : 'notifications-outline'
-                        } else if (rn == profileName) {
-                            iconName = focused ? 'person' : 'person-outline'
+                            return <Ionicons name={iconName} size={size} color={color} />
+                        },
+                        tabBarShowLabel: false,
+                        header: ({ navigation, route, options }) => {
+                            const title = getHeaderTitle(options, route.name);
+
+                            return <Header title={title} />
                         }
+                    })}
+                >
 
-                        return <Ionicons name={iconName} size={size} color={color} />
-                    }
-                })}
-            >
+                    <Tab.Screen name={homeName} component={Home} />
+                    <Tab.Screen name={searchName} component={SearchPage} />
+                    <Tab.Screen name={podcastName} component={Podcast} />
+                    <Tab.Screen name={notificationName} component={Notification} />
+                    {/* <Tab.Screen name={profileName} component={Profile} /> */}
 
-                <Tab.Screen name={homeName} component={Home} />
-                <Tab.Screen name={searchName} component={SearchPage} />
-                <Tab.Screen name={podcastName} component={Podcast} />
-                <Tab.Screen name={notificationName} component={Notification} />
-                {/* <Tab.Screen name={profileName} component={Profile} /> */}
+                </Tab.Navigator>
+                {/* <Stack.Navigator>
+                    <Stack.Group screenOptions={{ presentation: 'modal' }}>
+                        <Stack.Screen name="postmodal" component={PostModal} />
+                    </Stack.Group>
+                </Stack.Navigator> */}
 
-            </Tab.Navigator>
-        </NavigationContainer>
+            </NavigationContainer>
 
+        </>
     )
 }
 
