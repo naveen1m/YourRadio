608511a038375f2e71880664bd591ce386ee5b9d
naveen <stupiddint@gmail.com>
2023-10-25 15:48:31 +0530

    updated and added new code lines

--- frontend/src/NativeNavigation.jsx
+++ frontend/src/NativeNavigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { getHeaderTitle } from '@react-navigation/elements';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -14,56 +15,65 @@ const searchName = 'Search'
 const podcastName = 'Podcast'
 const notificationName = 'Notifications'
 
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
+function TabNavigator() {
+    return (
+        <Tab.Navigator
+            initialRouteName={homeName}
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    let rn = route.name;
+
+                    if (rn == homeName) {
+                        iconName = focused ? 'home' : 'home-outline'
+                    } else if (rn == searchName) {
+                        iconName = focused ? 'search' : 'search-outline'
+                    } else if (rn == podcastName) {
+                        iconName = focused ? 'radio' : 'radio-outline'
+                    } else if (rn == notificationName) {
+                        iconName = focused ? 'notifications' : 'notifications-outline'
+                    } else if (rn == profileName) {
+                        iconName = focused ? 'person' : 'person-outline'
+                    }
+
+                    return <Ionicons name={iconName} size={size} color={color} />
+                },
+                tabBarShowLabel: false,
+                header: ({ navigation, route, options }) => {
+                    const title = getHeaderTitle(options, route.name);
+
+                    return <Header title={title} />
+                }
+            })}
+        >
+
+            <Tab.Screen name={homeName} component={Home} />
+            <Tab.Screen name={searchName} component={StackNavigator} options={{ headerShown: false }} />
+            <Tab.Screen name={podcastName} component={Podcast} />
+            <Tab.Screen name={notificationName} component={Notification} />
+            {/* <Tab.Screen name={profileName} component={Profile} /> */}
+
+        </Tab.Navigator>
+    )
+}
+
+function StackNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name='Homescreen' component={SearchPage} options={{ headerShown: false }} />
+            <Stack.Screen name='profile' component={Profile} />
+        </Stack.Navigator>
+    )
+}
 function NativeNavigation() {
-    const Tab = createBottomTabNavigator();
-    // const Stack = createStackNavigator();
+
     return (
         <>
             <NavigationContainer>
-                <Tab.Navigator
-                    initialRouteName={homeName}
-                    screenOptions={({ route }) => ({
-                        tabBarIcon: ({ focused, color, size }) => {
-                            let iconName;
-                            let rn = route.name;
-
-                            if (rn == homeName) {
-                                iconName = focused ? 'home' : 'home-outline'
-                            } else if (rn == searchName) {
-                                iconName = focused ? 'search' : 'search-outline'
-                            } else if (rn == podcastName) {
-                                iconName = focused ? 'radio' : 'radio-outline'
-                            } else if (rn == notificationName) {
-                                iconName = focused ? 'notifications' : 'notifications-outline'
-                            } else if (rn == profileName) {
-                                iconName = focused ? 'person' : 'person-outline'
-                            }
-
-                            return <Ionicons name={iconName} size={size} color={color} />
-                        },
-                        tabBarShowLabel: false,
-                        header: ({ navigation, route, options }) => {
-                            const title = getHeaderTitle(options, route.name);
-
-                            return <Header title={title} />
-                        }
-                    })}
-                >
-
-                    <Tab.Screen name={homeName} component={Home} />
-                    <Tab.Screen name={searchName} component={SearchPage} options={{ headerShown: false }} />
-                    <Tab.Screen name={podcastName} component={Podcast} />
-                    <Tab.Screen name={notificationName} component={Notification} />
-                    {/* <Tab.Screen name={profileName} component={Profile} /> */}
-
-                </Tab.Navigator>
-                {/* <Stack.Navigator>
-                    <Stack.Group screenOptions={{ presentation: 'modal' }}>
-                        <Stack.Screen name="postmodal" component={PostModal} />
-                    </Stack.Group>
-                </Stack.Navigator> */}
-
+                <TabNavigator />
             </NavigationContainer>
 
         </>
--- frontend/src/screens/Profile.jsx
+++ frontend/src/screens/Profile.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { View } from '@gluestack-ui/themed'
-import { Text, Box } from '@gluestack-ui/themed'
+import {
+    Text,
+    Box,
+    Avatar,
+    AvatarBadge,
+    AvatarFallbackText,
+    AvatarImage,
+    VStack, HStack, Heading,
+} from '@gluestack-ui/themed';
+import HorizontalLine from '../components/HorizontalLine';
+import PostPodcastTab from '../components/PostPodcastTab';
 function Profile() {
     return (
-        <View>
-            <Box width="100%" height="100%" justifyContent="center" alignItems="center" bg="$green500" >
-                <Text color="$green" fontWeight="$bold" fontSize={"$2xl"} bg="$white" padding="$1.5" paddingTop="$10" >Profile Page</Text>
-            </Box>
-        </View>
+        <Fragment>
+            <View>
+                <Box mx={2} my={2}>
+                    <Heading>Username</Heading>
+                    <Box justifyContent='space-between' flexDirection='row'>
+                        <Avatar bgColor="$amber600" size="lg" borderRadius="$full" >
+                            <AvatarFallbackText>Ronald Richards</AvatarFallbackText>
+                            <AvatarImage
+                                source={{
+                                    uri: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60",
+                                }}
+                            />
+                        </Avatar>
+                        <HStack>
+                            <VStack mx={7} alignItems='center' >
+                                <Text>10</Text>
+                                <Text>Post</Text>
+                            </VStack>
+                            <VStack mx={7} alignItems='center' >
+                                <Text>50</Text>
+                                <Text>Follower</Text>
+                            </VStack>
+                            <VStack mx={7} alignItems='center' >
+                                <Text>90</Text>
+                                <Text>Following</Text>
+                            </VStack>
+                        </HStack>
+                    </Box>
+                    <VStack marginBottom={3}>
+                        <Heading size="sm">Ronald Richards</Heading>
+                    </VStack>
+                    <Text>Bio....</Text>
+                </Box>
+                <HorizontalLine />
+
+                <Box>
+                    <PostPodcastTab />
+                </Box>
+            </View>
+        </Fragment>
     )
 }
 
