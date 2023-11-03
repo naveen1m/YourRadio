92f977e00a7fed46f4f9c8b515e3bb047dc12532
naveen <stupiddint@gmail.com>
2023-11-03 15:35:27 +0530

    removed login code

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,10 +1,19 @@
 import 'react-native-gesture-handler';
 import 'expo-dev-client'
+import { GluestackUIProvider } from '@gluestack-ui/themed'
+import { config } from "@gluestack-ui/config"
 import Main from "./src/Main";
+import { UserContext } from './src/context/UserContext';
+import { useState } from 'react';
 
 
 export default function App() {
+  const [user, setUser] = useState();
   return (
-    <Main />
+    <GluestackUIProvider config={config}>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Main />
+      </UserContext.Provider>
+    </GluestackUIProvider>
   );
 }
