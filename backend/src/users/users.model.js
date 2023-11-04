b2adbc94e2386f038c99657cf937b01247bb766b
naveen <stupiddint@gmail.com>
2023-11-04 10:17:13 +0530

    users route and controller

---
+++ backend/src/users/users.model.js
@@ -0,0 +1,34 @@
+import mongoose, { Schema } from "mongoose";
+
+const { ObjectId } = mongoose.Schema.Types;
+const UserSchema = new Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: Buffer,
+    },
+    banner: {      // for notification page
+        type: Buffer,
+    },
+    followers: [],
+    following: [],
+    // followers: [{ type: ObjectId, ref: "User" }],
+    // following: [{ type: ObjectId, ref: "User" }],
+})
+
+const UserModel = mongoose.model("User", UserSchema)
+export default UserModel;
---
+++ backend/src/users/users.route.js
@@ -0,0 +1,122 @@
+import { Router } from "express";
+import UserModel from "./users.model.js";
+
+const userRouter = Router();
+
+/** Create new user */
+userRouter.post('/create', async (req, res) => {
+    console.log('called user/create')
+    const { displayName, email, uid } = req.body;
+
+    const newUser = UserModel({
+        name: displayName,
+        email: email,
+        _id: uid
+
+    })
+    try {
+        await newUser.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+
+    }
+
+})
+/** Get a user details */
+userRouter.get('/get/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const user = await UserModel.findById(id);
+        if (user) {
+            res.status(200).json(user);
+        } else {
+            res.status(404).json({ message: "no such user exist" });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+
+    }
+})
+
+/** Update a user details */
+userRouter.post('/get/:id', async (req, res) => {
+    const id = req.params.id;
+    const { name, username, photo, banner } = req.body;
+    try {
+        const user = await UserModel.findByIdAndUpdate(id, req.body, {
+            new: true,
+        });
+        if (!user) {
+            res.status(404).json({ message: "no such user exist" });
+        }
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+
+    }
+})
+/** Follow a user */
+userRouter.put('/follow/:id', async (req, res) => {
+    const id = req.params.id;
+    const { currentUserId } = req.body;
+
+    if (currentUserId === id) {
+        res.status(403).json({ message: "Action forbidden" });
+    } else {
+        try {
+            const followUser = await UserModel.findById(id);
+            const followingUser = await UserModel.findById(currentUserId);
+
+            if (!followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({ $push: { followers: currentUserId } });
+                await followingUser.updateOne({ $push: { following: id } });
+                res.status(200).json({ message: "User followed!" });
+            } else {
+                res.status(403).json({ message: "User is Already followed by you" });
+            }
+
+        } catch (error) {
+            res.status(500).json(error);
+
+        }
+    }
+
+});
+userRouter.put('/unfollow/:id', async (req, res) => {
+    const id = req.params.id;
+    const { currentUserId } = req.body;
+
+    if (currentUserId === id) {
+        res.status(403).json("Action forbidden");
+    } else {
+        const followUser = await UserModel.findById(id);
+        const followingUser = await UserModel.findById(currentUserId);
+        if (followUser.followers.includes(currentUserId)) {
+            await followUser.updateOne({ $pull: { followers: currentUserId } });
+            await followingUser.updateOne({ $pull: { following: id } });
+            res.status(200).json({ message: "User Unfollowed!" });
+        } else {
+            res.status(403).json({ message: "User is not followed by you" });
+        }
+    }
+});
+
+userRouter.post('/usersearch', async (req, res) => {
+    const user = req.query;
+
+
+});
+/** delete a user */
+userRouter.get('/get/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        await UserModel.findByIdAndDelete(id);
+        res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+
+    }
+})
+export default userRouter;
