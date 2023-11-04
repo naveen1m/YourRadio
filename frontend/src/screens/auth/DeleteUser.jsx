52d0a30956253d4fd6e254264d118e0a7e0754e8
naveen <stupiddint@gmail.com>
2023-11-04 10:19:10 +0530

    delete user logic

---
+++ frontend/src/screens/auth/DeleteUser.jsx
@@ -0,0 +1,64 @@
+import { View, Text } from "@gluestack-ui/themed";
+import React, { useState, useEffect, useContext } from "react";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import auth from "@react-native-firebase/auth";
+import { UserContext } from "../../context/UserContext";
+import { TouchableOpacity } from "react-native";
+
+function DeleteUser() {
+    // Set an initializing state whilst Firebase connects
+    const [initializing, setInitializing] = useState(true);
+    const { user, setUser } = useContext(UserContext);
+
+    GoogleSignin.configure({
+        webClientId:
+            "1047977436439-u7kv2656ajnnmss3usma32nov8b4pe14.apps.googleusercontent.com",
+    });
+
+    // Handle user state changes
+    function onAuthStateChanged(user) {
+        setUser(user);
+        if (!initializing) setInitializing(true);
+    }
+
+    useEffect(() => {
+        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+        return subscriber; // unsubscribe on unmount
+    }, []);
+
+    const onGoogleButtonPress = async () => {
+        // TO DELETE USER
+        await auth()
+            .currentUser.delete()
+
+            .catch((err) => {
+                // @error — auth/requires-recent-login Thrown if the user's last sign-in time does not meet the security threshold.
+                // Use auth.User#reauthenticatAeWithCredential to resolve. This does not apply if the user is anonymous.
+                console.log(err);
+            });
+
+        await GoogleSignin.signOut();
+
+        onAuthStateChanged(null);
+    };
+
+    if (user) {
+        return (
+            <View>
+                <TouchableOpacity
+                    style={{
+                        width: 300,
+                        height: 40,
+                        marginTop: 10,
+                        backgroundColor: "lightgreen",
+                    }}
+                    onPress={onGoogleButtonPress}
+                >
+                    <Text>Delete User</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+export default DeleteUser;
