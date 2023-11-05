56758bb080e35400bfdac2411c7f6afece172ed8
naveen <stupiddint@gmail.com>
2023-11-05 15:11:30 +0530

    modified

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import connect from './src/database/mongoMemoryConnection.js'
 import morgan from 'morgan';
 import postRouter from './src/posts/posts.route.js';
 import userRouter from './src/users/users.route.js';
+import podcastRouter from './src/podcasts/podcast.route.js';
 
 
 const port = process.env.PORT || 3000
@@ -13,18 +14,13 @@ const app = express();
 app.use(json());
 app.use(cors({ origin: '*' }));
 app.use(morgan('tiny'))
-// app.disable('x-powered-by');
+app.disable('x-powered-by');
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next();
-// });
 
 // Mount the route handlers
 app.use('/post', postRouter);
 app.use('/user', userRouter)
+app.use("/podcast", podcastRouter);
 
 app.get('/', (req, res) => {
     res.status(200).json("hello there! are you listening me>>>")
