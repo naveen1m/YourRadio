97777337236e028054c8bc3c0511738147d2b5ed
naveen <stupiddint@gmail.com>
2023-11-05 21:58:40 +0530

    view post ui done

--- frontend/src/components/ViewPost.jsx
+++ frontend/src/components/ViewPost.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
 import { TouchableOpacity } from 'react-native'
-import { View, Heading, Text, Box, Image } from '@gluestack-ui/themed'
+import { View, Heading, Text, Box, Image, Center } from '@gluestack-ui/themed'
 
 const ViewPost = () => {
     return (
         <View>
-            <Box w="$auto" marginLeft={"$2"} marginRight={"$2"} bg="$blue300"  >
-                <Heading underline={true} size={"$xl"} bold={true} >Traffic Jam</Heading>
-                <View  >
-                    <Text>Oh God! Today was trash. I didn t expect to be home
-                        nearly. So much pain it was.
-                    </Text>
-                    <Text alignItems='flex-end' alignContent='flex-end' alignSelf='flex-end' >~ Dom Shiitic</Text>
+            <Box w="$auto" marginLeft={1} marginRight={1} paddingVertical={2} paddingBottom={6} marginBottom={3} bg="$blue300"  >
+                <Heading underline={true} size={"$xl"} bold={true} paddingLeft={7} >Traffic Jam</Heading>
+                <View flexDirection='row' >
+                    <View flex={3} paddingLeft={3}>
+                        <Text>Oh God! Today was trash. I didn t expect to be home
+                            nearly. So much pain it was.</Text>
+                        <Text alignItems='flex-end' alignContent='flex-end' alignSelf='flex-end' color='$green800' >~ Dom Shiitic</Text>
 
-
-                    <View alignItems='flex-end' alignContent='flex-end' alignSelf='flex-end'>
-                        <TouchableOpacity >
-                            <Image
-                                size="sm"
-                                borderRadius="$full"
-                                source={require('../../assets/playpause512.png')}
-                                alt='play-pause'
-                            />
-                        </TouchableOpacity>
+                    </View>
+                    <View flex={1}  >
+                        <Center>
+                            <TouchableOpacity >
+                                <Image
+                                    size="sm"
+                                    borderRadius="$full"
+                                    source={require('../../assets/playpause512.png')}
+                                    alt='play-pause'
+                                />
+                            </TouchableOpacity>
+                        </Center>
                     </View>
                 </View>
             </Box>
