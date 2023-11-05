92e7e3ad94c5470788f0d0042ff4bb7171e9f4dc
naveen <stupiddint@gmail.com>
2023-11-05 11:22:48 +0530

    axios instance

---
+++ frontend/src/config/axiosInstance.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const baseURL = "https://097b-2401-4900-3e91-2447-6789-1f20-d471-dad6.ngrok-free.app";
+
+const axiosInst = axios.create({ baseURL, timeout: 10000 });
+
+export default axiosInst;
