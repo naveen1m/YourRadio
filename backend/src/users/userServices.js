dd8cbb0c253029334c29cc7406edcf39687bd5ef
naveen <stupiddint@gmail.com>
2023-11-05 15:07:51 +0530

    Add: user services

---
+++ backend/src/users/userServices.js
@@ -0,0 +1,33 @@
+import User from "./users.model.js";
+import { default as Session } from "../models/session.model.js";
+
+export const checkUserExists = async (email, username) => {
+    let user = await User.findOne({
+        $or: [{ email: email }],
+    });
+    if (user) {
+        return { result: true, user: user };
+    } else {
+        return { result: false };
+    }
+};
+
+export const addUserToSession = async (user_id) => {
+    let user = await Session.findOne({ user: user_id });
+    if (user) {
+        await removeUserFromSession(user_id); // expires previous token
+    }
+    return await Session.create({ user: user_id });
+};
+
+export const removeUserFromSession = async (user_id) => {
+    await Session.findOneAndRemove({ user: user_id });
+};
+
+export const createUser = async ({ email, name, uid }) => {
+    return await User.create({ email, name, uid });
+};
+
+export const getSessionDetails = async (session_id) => {
+    return Session.findById(session_id).populate('user');
+};
