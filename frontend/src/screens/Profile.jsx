c8c0a6e276d3428c92235123a66f9c32437518be
naveen <stupiddint@gmail.com>
2023-11-07 19:48:05 +0530

    integrated with backend done

--- frontend/src/screens/Profile.jsx
+++ frontend/src/screens/Profile.jsx
@@ -1,9 +1,28 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useState, useEffect } from 'react';
 import { Center, Image, Input, InputField, View } from '@gluestack-ui/themed';
 import { Text, Box, Avatar, AvatarBadge, AvatarFallbackText, AvatarImage, VStack, HStack, Heading } from '@gluestack-ui/themed';
 import { ScrollView, TouchableOpacity } from 'react-native';
 import ViewPost from '../components/ViewPost';
+import { GlobalContext } from '../context/GlobalContext';
+import axiosInst from '../config/axiosInstance';
 function Profile({ navigation }) {
+    const { user, setUser } = useContext(GlobalContext);
+    const [userData, setUserData] = useState('');
+    // console.log(user.uid)
+
+    // const response = await axiosInst.get(`/user/get/${user.uid}`)
+
+    useEffect(() => {
+        axiosInst.get(`/user/get/${user.uid}`).then(value => {
+            // console.log(value.data);
+            setUser(value.data);
+        })
+    }, [])
+    console.log(userData)
+    const { name, email, about, tagline, followers, following, posts, userName } = user;
+
+    // console.log(name, about, tagline, email);
+
     return (
         <Fragment>
             <ScrollView>
@@ -14,12 +33,12 @@ function Profile({ navigation }) {
 
                             <View backgroundColor='#02CCFE' flexDirection='row' flex={3} marginTop={6} height={80}  >
                                 <View flexDirection='coloumn' >
-                                    <Heading underline={true} size={"$xl"} bold={true} paddingLeft={7}>Tasdid Shitab</Heading>
-                                    <Text paddingLeft={3}>~ Soccer lover with no regrets</Text>
+                                    <Heading underline={true} size={"$xl"} bold={true} paddingLeft={7}>{name ?? "Tasdid Shitab"}</Heading>
+                                    <Text paddingLeft={3}>~ {tagline ?? "Soccer lover with no regrets"}</Text>
                                 </View>
                             </View>
                             <Avatar bgColor='$amber600' size='xl' borderRadius='$sm' borderWidth={2} borderColor='#00F788' >
-                                <AvatarFallbackText>Ronald Richards</AvatarFallbackText>
+                                <AvatarFallbackText>{name ?? "Tasdid Shitab"}</AvatarFallbackText>
                                 <AvatarImage
                                     source={{
                                         uri: 'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
@@ -31,22 +50,22 @@ function Profile({ navigation }) {
                         </Box>
                         <HStack>
 
-                            <Text fontWeight='$bold' >10</Text>
+                            <Text fontWeight='$bold' >{posts ?? "10"}</Text>
                             <Text fontWeight='$bold' marginRight={8} > posts  •</Text>
 
-                            <Text fontWeight='$bold'>50</Text>
+                            <Text fontWeight='$bold'>{followers ?? "10"}</Text>
                             <Text fontWeight='$bold' marginRight={8}> follower  •</Text>
 
-                            <Text fontWeight='$bold'>90</Text>
+                            <Text fontWeight='$bold'>{following ?? "10"}</Text>
                             <Text fontWeight='$bold'> following</Text>
 
                         </HStack>
                         <VStack marginBottom={3} marginTop={4} borderBottomWidth={5} borderBottomColor='$black' />
 
                         <Text>
-                            Studies in Military Academia of Science & Technology
+                            {about ?? `Studies in Military Academia of Science & Technology
                             Studied in St.Mary School & College
-                            Went to Daniel Engineering College
+                            Went to Daniel Engineering College`}
                         </Text>
                         <VStack marginBottom={3} marginTop={4} borderBottomWidth={2} borderBottomColor='$black' borderStyle='dotted' />
 
