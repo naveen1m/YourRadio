1650e47f0572b38faa0e3eef2b3284dc97c4f2c9
naveen <stupiddint@gmail.com>
2023-11-07 20:30:11 +0530

    post services

--- backend/src/posts/postServices.js
+++ backend/src/posts/postServices.js
@@ -1,31 +1,39 @@
 import postModel from "./posts.model.js";
 import userModel from "../users/users.model.js";
+
 export const createPost = async (body) => {
     return await postModel.create(body);
+};
+
+export const linkPostIdToAccount = async (user_id, post_id) => {
+    return await userModel.findByIdAndUpdate(
+        user_id,
+        { $addToSet: { posts: post_id } },
+        { new: true }
+    );
 };
 
 export const getSinglePost = async (post_id) => {
     return await postModel.findById(post_id);
 };
 
+export const getAllMyPosts = async (user_id) => {
+    return await userModel
+        .findById(user_id, { posts: 1, _id: 0 })
+        .populate("posts");
+};
+
 export const getAllPosts = async (user_id) => {
-    //   let friends_followers = await userModel
-    //     .findById(user_id, { followers: 1, friends: 1 })
-    //     .populate({ path: "friends followers", populate: { path: "posts" } });
-
-    //   console.log(friends_followers);
-
-    //   let a = friends_followers.friends.forEach((friend) => {});
-
-    //   return friends_followers;
-
-    //   TODO: FRIENDS AND FW feed;
-    let posts = await userModel.findById(user_id, { posts: 1 });
-    return posts.posts;
+    return await postModel.find({});
 };
 
 export const addReaction = async (user_id, post_id) => {
-    let post = await postModel.findById(post_id, { like: 1 });
-    await post.updateOne({ $addToSet: { like: user_id } });
+    let post = await postModel.findByIdAndUpdate(
+        post_id,
+        {
+            $addToSet: { like: user_id },
+        },
+        { new: true }
+    );
     return post;
 };
